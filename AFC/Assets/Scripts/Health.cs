d199c6577a8e7cc0f06b81a9432f0a6dbd4ea487
westmathews <144268678+westmathews@users.noreply.github.com>
2024-10-17 12:49:44 -0700

    Killing because Kaleb was himself
    I got the script to kill things that lose all their health.

--- AFC/Assets/Scripts/Health.cs
+++ AFC/Assets/Scripts/Health.cs
@@ -4,15 +4,24 @@ public class Health : MonoBehaviour
 {
     public static float hp;
     public float hepo;
+    public MeshRenderer player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         hepo = 100f;
+        player.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(hepo < 1)
+        {
+            Debug.Log("OH SHIT OH FUCK IVE BEEN SHOT");
+            player.enabled = false;
+            hepo = 9999999;
+            
+        }
         
     }
 }
