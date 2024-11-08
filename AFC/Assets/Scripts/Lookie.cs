7dda47ee39e64f0526dcee91b025a3ad947b316b
westmathews <144268678+westmathews@users.noreply.github.com>
2024-11-08 13:48:19 -0800

    we be coilin

--- AFC/Assets/Scripts/Lookie.cs
+++ AFC/Assets/Scripts/Lookie.cs
@@ -7,8 +7,14 @@ public class Lookie : MonoBehaviour
     public float mouseSensitivity = 100f; // Mouse sensitivity
     public Transform playerBody; // Reference to the player's body for rotation
     public float xRotation = 0f; // Vertical rotation
-    //public GameObject Sticker;
-    //public Transform player;
+    public GameObject Sticker;
+    public Transform player;
+    public Vector3 starting;
+    public float recoilX;
+    public float recoilStrength; // How strong the recoil is
+    public float recoilResetSpeed; // Speed at which recoil resets
+    public float retimer;
+    public bool coilin = false;
     void Start()
     {
         // Lock the cursor to the center of the screen
@@ -24,14 +30,21 @@ public class Lookie : MonoBehaviour
         // Apply mouse movement to the camera's rotation
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clamp the vertical rotation
-
+        xRotation -= recoilX;
+        recoilX = Mathf.Lerp(recoilX, 0f, recoilResetSpeed * Time.deltaTime);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
-        //if (Sticker.GetComponent<PewPew>().maingun)
-        //{
-            //Vector3 targetPosition = player.position + player.TransformDirection();
-            //xRotation -= mouseY + 20;
-            //transform.localRotation = Quaternion.Lerp(transform.localRotation,,0f);
-        //}
+        retimer += Time.deltaTime;
+        if (Sticker.GetComponent<PewPew>().maingun)
+        {
+            recoilX += recoilStrength;
+            retimer = 0;
+            coilin = true;
+        }
+        if (retimer > .3 && coilin)
+        {
+            recoilX -= recoilStrength;
+            coilin = false;
+        }
     }
 }
